6d9e97456a9b05e72cbdf6efe47591ba1dcd4c25
agamgn <agamgn@163.com>
2019-08-17 16:18:39 +0800

    add swiper

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="recommend">
       <div class="recommend-content">
-        <div class="slider-wrapper">
+        <div v-if="recommends.length" class="slider-wrapper">
+            <slider>
+                <div v-for="item in recommends" :key="item.id">
+                    <a :href="item.linkUrl">
+                        <img :src="item.picUrl" >
+                    </a>
+                </div>
+            </slider>
         </div>
         <div class="recommend-list">
           <h1 class="list-title">热门歌单推荐</h1>
@@ -12,6 +19,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from '@/base/slider/slider'
 import {getRecommend} from '@/api/recommend'
 import {playlistMixin} from '@/common/js/mixin'
 import {ERR_OK} from '@/api/config'
@@ -30,10 +38,14 @@ export default {
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
+          console.log(res.data.slider)
           this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 
 }
